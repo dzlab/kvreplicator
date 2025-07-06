7b44da055ea19018c7c5e909e6616cfc3c010604
dzlab <dzlabs@outlook.com>
2025-07-05 18:43:17 -0700

    implement fmt.Stringer interface in InternalKeyKind

--- internal/base/internal.go
+++ internal/base/internal.go
@@ -60,6 +60,31 @@ func (s SeqNum) SafeFormat(w redact.SafePrinter, _ rune) {
 // value, a merged value, etc.
 type InternalKeyKind uint8
 
+var internalKeyKindNames = []string{
+	InternalKeyKindDelete:         "DEL",
+	InternalKeyKindSet:            "SET",
+	InternalKeyKindMerge:          "MERGE",
+	InternalKeyKindLogData:        "LOGDATA",
+	InternalKeyKindSingleDelete:   "SINGLEDEL",
+	InternalKeyKindRangeDelete:    "RANGEDEL",
+	InternalKeyKindSeparator:      "SEPARATOR",
+	InternalKeyKindSetWithDelete:  "SETWITHDEL",
+	InternalKeyKindRangeKeySet:    "RANGEKEYSET",
+	InternalKeyKindRangeKeyUnset:  "RANGEKEYUNSET",
+	InternalKeyKindRangeKeyDelete: "RANGEKEYDEL",
+	InternalKeyKindIngestSST:      "INGESTSST",
+	InternalKeyKindDeleteSized:    "DELSIZED",
+	InternalKeyKindExcise:         "EXCISE",
+	InternalKeyKindInvalid:        "INVALID",
+}
+
+func (k InternalKeyKind) String() string {
+	if int(k) < len(internalKeyKindNames) {
+		return internalKeyKindNames[k]
+	}
+	return fmt.Sprintf("UNKNOWN:%d", k)
+}
+
 // InternalKeyTrailer encodes a SeqNum and an InternalKeyKind.
 type InternalKeyTrailer uint64
 
@@ -115,6 +140,93 @@ const (
 	InternalKeyKindSet     InternalKeyKind = 1
 	InternalKeyKindMerge   InternalKeyKind = 2
 	InternalKeyKindLogData InternalKeyKind = 3
+	//InternalKeyKindColumnFamilyDeletion     InternalKeyKind = 4
+	//InternalKeyKindColumnFamilyValue        InternalKeyKind = 5
+	//InternalKeyKindColumnFamilyMerge        InternalKeyKind = 6
+
+	// InternalKeyKindSingleDelete (SINGLEDEL) is a performance optimization
+	// solely for compactions (to reduce write amp and space amp). Readers other
+	// than compactions should treat SINGLEDEL as equivalent to a DEL.
+	// Historically, it was simpler for readers other than compactions to treat
+	// SINGLEDEL as equivalent to DEL, but as of the introduction of
+	// InternalKeyKindSSTableInternalObsoleteBit, this is also necessary for
+	// correctness.
+	InternalKeyKindSingleDelete InternalKeyKind = 7
+	//InternalKeyKindColumnFamilySingleDelete InternalKeyKind = 8
+	//InternalKeyKindBeginPrepareXID          InternalKeyKind = 9
+	//InternalKeyKindEndPrepareXID            InternalKeyKind = 10
+	//InternalKeyKindCommitXID                InternalKeyKind = 11
+	//InternalKeyKindRollbackXID              InternalKeyKind = 12
+	//InternalKeyKindNoop                     InternalKeyKind = 13
+	//InternalKeyKindColumnFamilyRangeDelete  InternalKeyKind = 14
+	InternalKeyKindRangeDelete InternalKeyKind = 15
+	//InternalKeyKindColumnFamilyBlobIndex    InternalKeyKind = 16
+	//InternalKeyKindBlobIndex                InternalKeyKind = 17
+
+	// InternalKeyKindSeparator is a key used for separator / successor keys
+	// written to sstable block indexes.
+	//
+	// NOTE: the RocksDB value has been repurposed. This was done to ensure that
+	// keys written to block indexes with value "17" (when 17 happened to be the
+	// max value, and InternalKeyKindMax was therefore set to 17), remain stable
+	// when new key kinds are supported in Pebble.
+	InternalKeyKindSeparator InternalKeyKind = 17
+
+	// InternalKeyKindSetWithDelete keys are SET keys that have met with a
+	// DELETE or SINGLEDEL key in a prior compaction. This key kind is
+	// specific to Pebble. See
+	// https://github.com/cockroachdb/pebble/issues/1255.
+	InternalKeyKindSetWithDelete InternalKeyKind = 18
+
+	// InternalKeyKindRangeKeyDelete removes all range keys within a key range.
+	// See the internal/rangekey package for more details.
+	InternalKeyKindRangeKeyDelete InternalKeyKind = 19
+	// InternalKeyKindRangeKeySet and InternalKeyKindRangeUnset represent
+	// keys that set and unset values associated with ranges of key
+	// space. See the internal/rangekey package for more details.
+	InternalKeyKindRangeKeyUnset InternalKeyKind = 20
+	InternalKeyKindRangeKeySet   InternalKeyKind = 21
+
+	InternalKeyKindRangeKeyMin InternalKeyKind = InternalKeyKindRangeKeyDelete
+	InternalKeyKindRangeKeyMax InternalKeyKind = InternalKeyKindRangeKeySet
+
+	// InternalKeyKindIngestSST is used to distinguish a batch that corresponds to
+	// the WAL entry for ingested sstables that are added to the flushable
+	// queue. This InternalKeyKind cannot appear amongst other key kinds in a
+	// batch (with the exception of alongside InternalKeyKindExcise), or in an sstable.
+	InternalKeyKindIngestSST InternalKeyKind = 22
+
+	// InternalKeyKindDeleteSized keys behave identically to
+	// InternalKeyKindDelete keys, except that they hold an associated uint64
+	// value indicating the (len(key)+len(value)) of the shadowed entry the
+	// tombstone is expected to delete. This value is used to inform compaction
+	// heuristics, but is not required to be accurate for correctness.
+	InternalKeyKindDeleteSized InternalKeyKind = 23
+
+	// InternalKeyKindExcise is used to persist the Excise part of an IngestAndExcise
+	// to a WAL. An Excise is similar to a RangeDel+RangeKeyDel combined, in that it
+	// deletes all point and range keys in a given key range while also immediately
+	// truncating sstables to exclude this key span. This InternalKeyKind cannot
+	// appear amongst other key kinds in a batch (with the exception of alongside
+	// InternalKeyKindIngestSST), or in an sstable.
+	InternalKeyKindExcise InternalKeyKind = 24
+
+	// This maximum value isn't part of the file format. Future extensions may
+	// increase this value.
+	//
+	// When constructing an internal key to pass to DB.Seek{GE,LE},
+	// internalKeyComparer sorts decreasing by kind (after sorting increasing by
+	// user key and decreasing by sequence number). Thus, use InternalKeyKindMax,
+	// which sorts 'less than or equal to' any other valid internalKeyKind, when
+	// searching for any kind of internal key formed by a certain user key and
+	// seqNum.
+	InternalKeyKindMax InternalKeyKind = 24
+
+	// InternalKeyKindMaxForSSTable is the largest valid key kind that can exist
+	// in an SSTable. This should usually equal InternalKeyKindMax, except
+	// if the current InternalKeyKindMax is a kind that is never added to an
+	// SSTable or memtable (eg. InternalKeyKindExcise).
+	InternalKeyKindMaxForSSTable InternalKeyKind = InternalKeyKindDeleteSized
 
 	// Internal to the sstable format. Not exposed by any sstable iterator.
 	// Declared here to prevent definition of valid key kinds that set this bit.
